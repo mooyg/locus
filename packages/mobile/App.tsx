6e233e2b1c2746e6475d38dbf362865254e7c8f5
mooyg <mooybot@gmail.com>
2021-07-11 19:01:21 +0530

    init(mobile)

--- packages/mobile/App.tsx
+++ packages/mobile/App.tsx
@@ -1,31 +1,16 @@
-import { StatusBar } from 'react-native';
-import React from 'react';
-import { NativeBaseProvider, Box, Image } from 'native-base';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import React from "react";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { HomeScreen } from "./Components/HomeScreen";
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <SafeAreaProvider>
-        <SafeAreaView>
-          <Box
-            bg="#111827"
-            height="100%"
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-          >
-            <Box>
-              <Image
-                source={{ uri: 'https://i.ibb.co/Gc6Ymym/logo.png' }}
-                alt="logo"
-                width="40px"
-                height="40px"
-              />
-            </Box>
-          </Box>
-        </SafeAreaView>
-      </SafeAreaProvider>
-      <StatusBar backgroundColor="#4B5D67" />
-    </NativeBaseProvider>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
